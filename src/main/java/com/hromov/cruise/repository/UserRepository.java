c0c6ab4947e10c6b53947be3a7602c1bb5370291
m-hromov <hromov095w@gmail.com>
2022-12-13 13:03:59 +0200

    CrudRepository to JpaRepository and findByEmail method implementation

--- src/main/java/com/hromov/cruise/repository/UserRepository.java
+++ src/main/java/com/hromov/cruise/repository/UserRepository.java
@@ -1,14 +1,15 @@
 package com.hromov.cruise.repository;
 
 import com.hromov.cruise.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
 @Repository
-public interface UserRepository extends CrudRepository<User, Long> {
-    @Query("SELECT User FROM User AS u1 WHERE u1.email = us")
-    Optional<User> findByEmail(String email);
+public interface UserRepository extends JpaRepository<User, Long> {
+    @Query("SELECT u1 FROM User u1 WHERE u1.email = :email")
+    Optional<User> findByEmail(@Param("email") String email);
 }
