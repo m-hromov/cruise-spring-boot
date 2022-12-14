fd91c17a88a0697fa96978702a651d7f0f82a63d
m-hromov <hromov095w@gmail.com>
2022-12-14 22:06:06 +0200

    refactoring

--- src/main/java/com/hromov/cruise/repository/UserRepository.java
+++ src/main/java/com/hromov/cruise/repository/UserRepository.java
@@ -2,14 +2,11 @@ package com.hromov.cruise.repository;
 
 import com.hromov.cruise.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
-    @Query("SELECT u1 FROM User u1 WHERE u1.email = :email")
-    Optional<User> findByEmail(@Param("email") String email);
+    Optional<User> findByEmail(String email);
 }
